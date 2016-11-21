fd24465a10744ce6c8d133276e57e683c3c5a62a
virth <info@joshuawirth.ch>
2016-11-21 22:27:09 +0100

    fix mail and refactoring
    added trim() for the recipient so every email gets sendt
    did some refactoring, all code relevant in sending mails is now in
    sendMail()

--- logic/pdfgenerator.php
+++ logic/pdfgenerator.php
@@ -17,111 +17,87 @@
 	   	function generateSimpleTypeDE ($type, $data, $user, $menge)
 	   	{
 	   		require('simpleTypeDE.php');
-			//PDF generieren
 			$simpleTypeDE = new simpleTypeDE();
 			$pdf = $simpleTypeDE->generate($type, $data);		
-			//PDF speichern
-			$fname = $this->writePDF($pdf, $data[0], $menge);
-			//Liese Empfänger Adressen und schreibe E-Mails
-			$datei = "conf.dat"; // Name der Datei
-	    	$entrysArray = file($datei); // Datei in ein Array einlesen
-	    	$anzentrys = sizeof($entrysArray);
-	   		for ($i = 0; $i < $anzentrys; $i++) 
-	   		{
-				$this->sendMail($entrysArray[$i], $fname, $user, $menge);
-	   		}
+			$filename = $this->writePDF($pdf, $data[0], $menge);
+			$this->sendMail($filename, $user, $menge);
 	   	}
 
 	   	function generateTwoSided ($type, $data, $user, $menge)
 	   	{
 	   		require('twoSided.php');
-	   		//PDF generieren
 			$twoSided = new twoSided();
 			$pdf = $twoSided->generate($type, $data);		
-			//PDF speichern
-			$fname = $this->writePDF($pdf, $data[0], $menge);
-			//Liese Empfänger Adressen und schreibe E-Mails
-			$datei = "conf.dat"; // Name der Datei
-	    	$entrysArray = file($datei); // Datei in ein Array einlesen
-	    	$anzentrys = sizeof($entrysArray);
-	   		for ($i = 0; $i < $anzentrys; $i++) 
-	   		{
-				$this->sendMail($entrysArray[$i], $fname, $user, $menge);
-	   		}	
+			$filename = $this->writePDF($pdf, $data[0], $menge);
+			$this->sendMail($filename, $user, $menge);	   		
 	   	}
 	   	
 	   	function generateTwoSidedBackEditable($type, $dataFront, $dataBack, $user, $menge)
 	   	{
 	   		require('twoSidedBackEditable.php');
-	   		//PDF generieren
 			$twoSidedBackEditable = new twoSidedBackEditable();
 			$pdf = $twoSidedBackEditable->generate($type, $dataFront, $dataBack);		
-			//PDF speichern
-			$fname = $this->writePDF($pdf, $dataFront[0], $menge);
-			//Liese Empfänger Adressen und schreibe E-Mails
-			$datei = "conf.dat"; // Name der Datei
-	    	$entrysArray = file($datei); // Datei in ein Array einlesen
-	    	$anzentrys = sizeof($entrysArray);
-	   		for ($i = 0; $i < $anzentrys; $i++) 
-	   		{
-				$this->sendMail($entrysArray[$i], $fname, $user, $menge);
-	   		}	
+			$filename = $this->writePDF($pdf, $dataFront[0], $menge);
+			$this->sendMail($filename, $user, $menge);	   
 	   	}
 		
 		function generate601 ($type, $data, $user, $menge)
 	   	{
 	   		require('kkSimple01.php');
-			//PDF generieren
 			$kkSimple01 = new kkSimple01();
 			$pdf = $kkSimple01->generate($type, $data);		
-			//PDF speichern
-			$fname = $this->writePDF($pdf, $data[0], $menge);
-			//Liese Empfänger Adressen und schreibe E-Mails
-			$datei = "conf.dat"; // Name der Datei
-	    	$entrysArray = file($datei); // Datei in ein Array einlesen
-	    	$anzentrys = sizeof($entrysArray);
-	   		for ($i = 0; $i < $anzentrys; $i++) 
-	   		{
-				$this->sendMail($entrysArray[$i], $fname, $user, $menge);
-	   		}
+			$filename = $this->writePDF($pdf, $data[0], $menge);			
+			$this->sendMail($filename, $user, $menge);
 	   	}
 	   	
 		private function writePDF($pdfToWrite, $name, $anzahl)
 		{
 		    $now = date("_dmy(His)", strtotime(date('r')));
 			$filename = strtolower($name.$now."_".$anzahl.".pdf");
-			$filename = str_replace("ö", "oe", $filename);
-			$filename = str_replace("ä", "ae", $filename);
-			$filename = str_replace("ü", "ue", $filename);
+			$filename = str_replace("ï¿½", "oe", $filename);
+			$filename = str_replace("ï¿½", "ae", $filename);
+			$filename = str_replace("ï¿½", "ue", $filename);
 			$filename = str_replace(" ", "_", $filename);
 			$filename = urlencode ($filename);
 			$pdfToWrite->Output("vcards/".htmlspecialchars_decode($filename), "F");
 			return $filename;
 		}
 		
-		private function sendMail($empfaenger, $fname, $user, $menge)
+		private function sendMail($filename, $user, $menge)
 		{
-			$absendername = "VK PDF Generator";
-			$absendermail = "VK.Generator@e-druck.ch";
-			$betreff = "Bestellung";
-			$text = "Guten Tag \n\nSoeben ist eine Bestellung von User $user eingegangen. \n\nMenge: $menge \n\nPDF: http://kssg-visitenkarten.ch/vcards/".urlencode($fname);
-		
-			mail($empfaenger, $betreff, $text, "From: $absendername <$absendermail>");	
+	    	$recipients = file("conf.dat"); 
+	    	$countRecipients = sizeof($recipients);
+	   		for ($i = 0; $i < $countRecipients; $i++) 
+	   		{
+				$recipient = trim($recipients[$i]);
+				$pdfGenerator = new pdfGenerator();
+				$pdfGenerator->debugToConsole("send mail to [$recipient]");
+				$absendername = "VK PDF Generator";
+				$absendermail = "VK.Generator@e-druck.ch";
+				$betreff = "Bestellung";
+				$text = "Guten Tag \n\nSoeben ist eine Bestellung von User $user eingegangen. \n\nMenge: $menge \n\nPDF: http://kssg-visitenkarten.ch/vcards/".urlencode($filename);
+			
+				mail($recipient, $betreff, $text, "From: $absendername <$absendermail>");
+			}	
 		}
 
-public function mailcheck($mail) 
-	{
-	$pdfGenerator = new pdfGenerator();
-		$email = $pdfGenerator->customspecialchars($mail);
-		if (!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email) ) 
+		public function mailcheck($mail) 
 		{
-			return false;
+			$pdfGenerator = new pdfGenerator();
+			$email = $pdfGenerator->customspecialchars($mail);
+			if (!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email) ) 
+			{
+				return false;
+			}
+
+			return true;
 		}
 
-		return true;
-	}
-
-
+		function debugToConsole($data) 
+		{		
+			$output = "<script>console.log('".$data."');</script>";
+			echo $output;
+		}
 	}		
 		
 ?>
